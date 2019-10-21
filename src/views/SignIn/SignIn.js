7068a42292736425e506aa0ac6e8bedd30bae1af
Leandro Mello <leandro.mello@qualita.inf.br>
2019-10-21 18:37:07 -0300

    Initial commit

---
+++ src/views/SignIn/SignIn.js
@@ -0,0 +1,221 @@
+import React, { useState, useEffect } from 'react';
+import { Link as RouterLink, withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import validate from 'validate.js';
+import useStyles from './styles.js';
+import {
+  Grid,
+  Button,
+  // IconButton,
+  TextField,
+  Link,
+  Typography
+} from '@material-ui/core';
+// import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+
+const schema = {
+  email: {
+    presence: { allowEmpty: false, message: 'is required' },
+    email: true,
+    length: {
+      maximum: 64
+    }
+  },
+  password: {
+    presence: { allowEmpty: false, message: 'is required' },
+    length: {
+      maximum: 128
+    }
+  }
+};
+
+
+const SignIn = props => {
+  const { history } = props;
+
+  const classes = useStyles();
+
+  const [formState, setFormState] = useState({
+    isValid: false,
+    values: {},
+    touched: {},
+    errors: {}
+  });
+
+  useEffect(() => {
+    const errors = validate(formState.values, schema);
+
+    setFormState(formState => ({
+      ...formState,
+      isValid: errors ? false : true,
+      errors: errors || {}
+    }));
+  }, [formState.values]);
+
+  // const handleBack = () => {
+  //   history.goBack();
+  // };
+
+  const handleChange = event => {
+    event.persist();
+
+    setFormState(formState => ({
+      ...formState,
+      values: {
+        ...formState.values,
+        [event.target.name]:
+          event.target.type === 'checkbox'
+            ? event.target.checked
+            : event.target.value
+      },
+      touched: {
+        ...formState.touched,
+        [event.target.name]: true
+      }
+    }));
+  };
+
+  const handleSignIn = event => {
+    event.preventDefault();
+    history.push('/dashboard');
+  };
+
+  const hasError = field =>
+    formState.touched[field] && formState.errors[field] ? true : false;
+
+  return (
+    <div className={classes.root}>
+      <Grid
+        className={classes.grid}
+        container
+      >
+        <Grid
+          className={classes.quoteContainer}
+          item
+          lg={5}
+        >
+          <div className={classes.quote}>
+            <div className={classes.quoteInner}>
+              <Typography
+                className={classes.quoteText}
+                variant="h1"
+              >
+                Hella narwhal Cosby sweater McSweeney's, salvia kitsch before
+                they sold out High Life.
+              </Typography>
+              <div className={classes.person}>
+                <Typography
+                  className={classes.name}
+                  variant="body1"
+                >
+                  Takamaru Ayako
+                </Typography>
+                <Typography
+                  className={classes.bio}
+                  variant="body2"
+                >
+                  Manager at inVision
+                </Typography>
+              </div>
+            </div>
+          </div>
+        </Grid>
+        <Grid
+          className={classes.content}
+          item
+          lg={7}
+          xs={12}
+        >
+          <div className={classes.content}>
+            {/* <div className={classes.contentHeader}>
+              <IconButton onClick={handleBack}>
+                <ArrowBackIcon />
+              </IconButton>
+            </div> */}
+            <div className={classes.contentBody}>
+              <form
+                className={classes.form}
+                onSubmit={handleSignIn}
+              >
+                <Typography
+                  className={classes.title}
+                  variant="h2"
+                >
+                  Sign in
+                </Typography>
+              
+                
+                <Typography
+                  align="left"
+                  className={classes.sugestion}
+                  color="textSecondary"
+                  variant="body1"
+                >
+                  Login with email address
+                </Typography>
+                <TextField
+                  className={classes.textField}
+                  error={hasError('email')}
+                  fullWidth
+                  helperText={
+                    hasError('email') ? formState.errors.email[0] : null
+                  }
+                  label="Email address"
+                  name="email"
+                  onChange={handleChange}
+                  type="text"
+                  value={formState.values.email || ''}
+                  variant="outlined"
+                />
+                <TextField
+                  className={classes.textField}
+                  error={hasError('password')}
+                  fullWidth
+                  helperText={
+                    hasError('password') ? formState.errors.password[0] : null
+                  }
+                  label="Password"
+                  name="password"
+                  onChange={handleChange}
+                  type="password"
+                  value={formState.values.password || ''}
+                  variant="outlined"
+                />
+                <Button
+                  className={classes.signInButton}
+                  color="primary"
+                  disabled={!formState.isValid}
+                  fullWidth
+                  size="large"
+                  type="submit"
+                  variant="contained"
+                >
+                  Sign in now
+                </Button>
+                <Typography
+                  color="textSecondary"
+                  variant="body1"
+                >
+                  Esqueceu sua senha?{' '}
+                  <Link
+                    component={RouterLink}
+                    to="/sign-up"
+                    variant="h6"
+                  >
+                    Recuperar
+                  </Link>
+                </Typography>
+              </form>
+            </div>
+          </div>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+SignIn.propTypes = {
+  history: PropTypes.object
+};
+
+export default withRouter(SignIn);
